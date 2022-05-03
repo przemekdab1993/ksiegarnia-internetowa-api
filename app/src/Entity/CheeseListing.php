4c6bf37c8bab36bd55779d8f54ed5bf01f7c8921
przemekdab1993 <przemekdab1993@gmail.com>
2022-05-03 12:16:01 +0200

    serialization groups

--- app/src/Entity/CheeseListing.php
+++ app/src/Entity/CheeseListing.php
@@ -3,35 +3,49 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 use App\Repository\CheeseListingRepository;
 use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CheeseListingRepository::class)]
-#[ApiResource(collectionOperations: ['get', 'post' ], itemOperations: ['get', 'put',], shortName: 'cheeses',)]
+/**
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={"get", "put"},
+ *     normalizationContext={"groups"={"cheeses_list:read"}, "swagger_definition_name"="Read"},
+ *     denormalizationContext={"groups"={"cheeses_list:write"}, "swagger_definition_name"="Write"},
+ *     shortName="cheeses"
+ * )
+ */
 class CheeseListing
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups('cheeses_list:read')]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['cheeses_list:read', 'cheeses_list:write'])]
     private $title;
 
     #[ORM\Column(type: 'text')]
+    #[Groups('cheeses_list:read')]
     private $description;
 
     #[ORM\Column(type: 'integer')]
+    #[Groups(['cheeses_list:read', 'cheeses_list:write'])]
     private $price;
 
     #[ORM\Column(type: 'datetime')]
     private $createdAt;
 
     #[ORM\Column(type: 'boolean')]
-    private $isPublished;
+    private $isPublished = false;
 
     #[ORM\Column(type: 'integer')]
+    #[Groups(['cheeses_list:read', 'cheeses_list:write'])]
     private $quantity;
 
 
@@ -61,7 +75,14 @@ class CheeseListing
     {
         return $this->description;
     }
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
 
+        return $this;
+    }
+
+    #[Groups('cheeses_list:write')]
     public function setTextDescription(string $description): self
     {
         $this->description = nl2br($description);
@@ -86,6 +107,8 @@ class CheeseListing
         return $this->createdAt;
     }
 
+
+    #[Groups('cheeses_list:read')]
     public function getCreatedAgo(): string
     {
         return Carbon::instance($this->getCreatedAt())->diffForHumans();
